acdc7c202be80fe9c6aee64bae675f69a91f1710
fredricngo <fredricngo@uchicago.edu>
2025-10-08 18:51:28 -0500

    Got some of MOVZ working

--- lab1/src/sim.c
+++ lab1/src/sim.c
@@ -1,6 +1,50 @@
 #include <stdio.h>
 #include "shell.h"
 
+typedef enum {
+    UNKNOWN = 0,
+    ADD_EXT,
+    ADD_IMM, 
+    ADDS_EXT, 
+    ADDS_IMM,
+    CBNZ, 
+    CBZ, 
+    AND_SHIFTR,
+    ANDS_SHIFTR,
+    EOR_SHIFTR, 
+    ORR_SHIFTR, 
+    LDUR,
+    LDURB, 
+    LDURH, 
+    LSL_IMM,
+    LSR_IMM, 
+    MOVZ,
+    STUR, 
+    STURB, 
+    STURH, 
+    SUB_EXT, 
+    SUB_IMM,
+    SUBS_EXT, 
+    SUBS_IMM, 
+    MUL, 
+    HLT, 
+    CMP_EXT, 
+    CMP_IMM, 
+    BR,
+    B, 
+    BEQ, 
+    BNE, 
+    BGT, 
+    BLT, 
+    BGE, 
+    BLE
+} instruction_type_t;
+
+instruction_type_t instruction_type;
+uint32_t current_instruction;
+uint32_t rd, rn, rm; 
+uint32_t immediate; 
+uint32_t shift_amount;
 
 uint32_t extract_bits(uint32_t instruction, int start, int end){
     //Given an instruction type, returns a section from start:end (inclusive)
@@ -12,7 +56,7 @@ uint32_t extract_bits(uint32_t instruction, int start, int end){
 void fetch()
 {
     //Given the instruction address, we retrieve from memory using mem_read_32
-    uint32_t instruction = mem_read_32(CURRENT_STATE.PC);
+    current_instruction = mem_read_32(CURRENT_STATE.PC);
 
 
 }
@@ -20,7 +64,8 @@ void fetch()
 void decode()
 {
     //Extract the operands using extract_bits, need to check opcodes and specific bit orientations
-    //Need to update the registers 
+    //Need to update the registers
+    instruction_type = UNKNOWN;
 
     //ADD(EXTENDED)
 
@@ -48,13 +93,11 @@ void decode()
 
     //LSR (IMM)
 
-    //MOVZ: shift
-    if (!(extract_bits(instruction, 23, 30) ^ 0xA5)){
-        uint32_t movz_rd = extract_bits(instruction, 0, 4);
-        uint32_t imm = extract_bits(instruction, 5, 20);
-        uint32_t hw = extract_bits(instruction, 21, 22);
-        uint32_t opc = extract_bits(instruction, 29, 30); 
-        uint32_t sf = extract_bits(instruction, 31, 31);
+    //MOVZ: !!Shift not required!! 
+    if (!(extract_bits(current_instruction, 23, 30) ^ 0xA5)){
+        instruction_type = MOVZ;
+        rd = extract_bits(current_instruction, 0, 4);
+        immediate = extract_bits(current_instruction, 5, 20);
     }
 
     //STUR (32-AND 64-BIT VAR)
@@ -92,6 +135,19 @@ void decode()
 
 void execute()
 {
+    //Checks the global instruction parameters, then performs the relevant operations
+    switch(instruction_type) {
+
+        case MOVZ:
+            if (extract_bits(current_instruction, 31, 31)){
+                //we update NEW_STATE registers
+                //We move the immediate into the register
+                //only need to implement 64-bit variant, no shift needed
+                NEXT_STATE.REGS[rd] = (uint64_t) immediate;
+            }
+            break;
+    }
+
 
 }
 
