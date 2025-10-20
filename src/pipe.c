a05a413203badaf2e53d0421dfe1b3fa647bca47
Fredric Lorenz Suson Ngo <fredricngo@focal2.cs.uchicago.edu>
2025-10-20 18:09:12 -0500

    Set up lab 2

---
+++ src/pipe.c
@@ -0,0 +1,55 @@
+/*
+ * CMSC 22200
+ *
+ * ARM pipeline timing simulator
+ */
+
+#include "pipe.h"
+#include "shell.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+
+/* global pipeline state */
+Pipe_State pipe;
+
+int RUN_BIT;
+
+void pipe_init()
+{
+    memset(&pipe, 0, sizeof(Pipe_State));
+    pipe.PC = 0x00400000;
+	RUN_BIT = true;
+}
+
+void pipe_cycle()
+{
+	pipe_stage_wb();
+	pipe_stage_mem();
+	pipe_stage_execute();
+	pipe_stage_decode();
+	pipe_stage_fetch();
+}
+
+
+
+void pipe_stage_wb()
+{
+}
+
+void pipe_stage_mem()
+{
+}
+
+void pipe_stage_execute()
+{
+}
+
+void pipe_stage_decode()
+{
+}
+
+void pipe_stage_fetch()
+{
+}
---
+++ src/pipe.h
@@ -0,0 +1,51 @@
+/*
+ * CMSC 22200
+ *
+ * ARM pipeline timing simulator
+ */
+
+#ifndef _PIPE_H_
+#define _PIPE_H_
+
+#include "shell.h"
+#include "stdbool.h"
+#include <limits.h>
+
+/* Represents an operation travelling through the pipeline. */
+typedef struct Pipe_Op {
+
+	/* place other information here as necessary */
+} Pipe_Op;
+
+/* Represents the current state of the pipeline. */
+typedef struct Pipe_State {
+	/* register file state */
+	int64_t REGS[ARM_REGS];
+	int FLAG_N;        /* flag N */
+	int FLAG_Z;        /* flag Z */
+
+	/* program counter in fetch stage */
+	uint64_t PC;
+
+	/* place other information here as necessary */
+} Pipe_State;
+
+extern int RUN_BIT;
+
+/* global variable -- pipeline state */
+extern Pipe_State pipe;
+
+/* called during simulator startup */
+void pipe_init();
+
+/* this function calls the others */
+void pipe_cycle();
+
+/* each of these functions implements one stage of the pipeline */
+void pipe_stage_fetch();
+void pipe_stage_decode();
+void pipe_stage_execute();
+void pipe_stage_mem();
+void pipe_stage_wb();
+
+#endif
