1cec96055000fb2cd3899fc674ca647e954665e8
Kayla Isabelle Wanahita <kwanahita@focal4.cs.uchicago.edu>
2025-10-29 11:48:27 -0500

    fixed br test

--- lab2/inputs/jitbr.s
+++ lab2/inputs/jitbr.s
@@ -1,11 +1,13 @@
 .text
-mov X1, #target_addr 
-mov X5, #0 
-mov X6, #0
-mov X7, #0
-mov X8, #0  
-br X1                     
-mov X0, #100             
+movz    x1, 0x40        // x1 = 0x0000000000000040
+lsl     x1, x1, #16     // x1 = 0x0000000000400000
+add     x1, x1, #0x1C   // x1 = 0x000000000040001C (address of target_addr)
+mov     x5, #0
+mov     x6, #0
+mov     x7, #0
+mov     x8, #0
+br      x1 
+mov     x0, #100
 target_addr:
-mov X0, #200              
-HLT 0
+mov     x0, #200
+HLT     0
